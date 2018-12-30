2967c29599aa0302c5cff3519860ebdcb17753d9
ram <sandhyasharma0423@gmail.com>
2018-12-30 17:04:52 +0530

    commit till template

---
+++ Shopping/src/main/java/org/sandhya/Shopping/controller/PageController.java
@@ -0,0 +1,20 @@
+package org.sandhya.Shopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController
+{
+	
+	@RequestMapping(value={"/","/index","/home"})
+	public ModelAndView index()
+	{
+		ModelAndView mv =new ModelAndView("page");
+		mv.addObject("message","welcome greetings");
+		return mv;
+	}
+	
+
+}
